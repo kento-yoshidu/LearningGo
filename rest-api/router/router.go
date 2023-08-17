5fab696655284d95497b7323b853de83f821f914
kento <kento@toriwatari.bird>
2023-08-17 12:41:13 +0900

    ⚡ Update

--- rest-api/router/router.go
+++ rest-api/router/router.go
@@ -45,38 +45,3 @@ func NewRouter(uc controller.IUserController, tc controller.ITaskController) *ec
 	t.DELETE("/:taskId", tc.DeleteTask)
 	return e
 }
-
-/*
-package router
-
-import (
-	"os"
-	"rest-api/controller"
-
-	echojwt "github.com/labstack/echo-jwt/v4"
-	"github.com/labstack/echo/v4"
-)
-
-func NewRouter(uc controller.IUserController, tc controller.ITaskController) *echo.Echo {
-	e := echo.New()
-	e.POST("/signup", uc.SignUp)
-	e.POST("/login", uc.LogIn)
-	e.POST("/logout", uc.LogOut)
-	// e.GET("/csrf", uc.CsrfToken)
-	t := e.Group("/tasks")
-	t.Use(echojwt.WithConfig(echojwt.Config{
-		SigningKey:  []byte(os.Getenv("SECRET")),
-		TokenLookup: "cookie:token",
-	}))
-	t.Use(echojwt.WithConfig(echojwt.Config{
-		SigningKey:  []byte(os.Getenv("SECRET")),
-		TokenLookup: "cookie:token",
-	}))
-	t.GET("", tc.GetAllTasks)
-	t.GET("/:taskId", tc.GetTaskById)
-	t.POST("", tc.CreateTask)
-	t.PUT("/:taskId", tc.UpdateTask)
-	t.DELETE("/:taskId", tc.DeleteTask)
-	return e
-}
-*/
