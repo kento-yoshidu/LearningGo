a245e0e2e27c21873ab022e00275cd1ddcc58bfb
kento <kento@toriwatari.bird>
2023-12-03 12:59:07 +0900

    🍈 Update

---
+++ monkey/lexer/lexer.go
@@ -0,0 +1,25 @@
+package lexer
+
+type Lexer struct {
+	input        string
+	position     int  // 現在の位置
+	readPosition int  // 次に読み込む位値(現在の文字の次の文字)
+	ch           byte // 現在検査中の文字
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+	return l
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+
+	l.position = l.readPosition
+	l.readPosition += 1
+}
